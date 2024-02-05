a98570df6a6cfb26b40ec64a00b5e71098b3e1ad
Pranshu_Patel <75875055+Pranshu-Patel@users.noreply.github.com>
2024-02-04 22:29:29 -0500

    Update pack.c

--- datatypes/pack.c
+++ datatypes/pack.c
@@ -1,38 +1,76 @@
-#include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+#include "mpi.h"
+int main(int argc, char* argv[])
+{
+    int my_rank;
+    int p;
+    int source;
+    int dest;
+    int tag=0;
+    int i;
+    int n=8;
+    int *Arr;
+    int j;
+    MPI_Datatype derived;
+    MPI_Status status;
+    int buffer[100];
+    int position;
 
-int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
 
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    // Create a square matrix (for example, 4x4)
-    int matrix[4][4];
-    // Initialize matrix with values
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-    // Calculate the size needed for packing
-    int packSize;
-    MPI_Pack_size(4, MPI_INT, MPI_COMM_WORLD, &packSize);
+    Arr=(int*)malloc(n*n*sizeof(int));
 
-    // Create a buffer for packing
-    char buffer[packSize];
-
-    // Pack the diagonal entries into the buffer
-    if (rank == 0) {
-        printf("Sending diagonal entry\n");
-        MPI_Pack(&matrix[0][0], 1, MPI_INT, buffer, packSize, &packSize, MPI_COMM_WORLD);
-        MPI_Send(buffer, packSize, MPI_PACKED, 1, 0, MPI_COMM_WORLD);
-    } else if (rank == 1) {
-        printf("Receiving diagonal entry\n");
-        MPI_Recv(buffer, packSize, MPI_PACKED, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        int receivedDiagonal;
-        int position = 0;
-        MPI_Unpack(buffer, packSize, &position, &receivedDiagonal, 1, MPI_INT, MPI_COMM_WORLD);
-        // Process 1 now has the diagonal entry
+    for(i=0;i<n*n;i++){
+        Arr[i]=my_rank;
     }
 
+    if(my_rank==0){
+        position=1;
+        printf("Matrix in process 0:\n");
+        for(i=0;i<n;i++){
+            MPI_Pack(&Arr[(i*n)+i],1,MPI_INT,buffer,100,&position,MPI_COMM_WORLD);
+            for(j=0;j<n;j++){
+                printf("%d ",Arr[(i*n)+j]);
+            }
+            printf("\n");
+        }
+        MPI_Send(buffer,100,MPI_PACKED,1,tag,MPI_COMM_WORLD);
+    }
+    else
+    if(my_rank==1){
+        position=0;
+        sleep(1);
+        printf("\nMatrix in process 1:\n");
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                printf("%d ",Arr[(i*n)+j]);
+            }
+            printf("\n");
+        }
+        MPI_Recv(buffer,100,MPI_PACKED,0,tag,MPI_COMM_WORLD,&status);
+        for(i=0;i<n;i++){
+            MPI_Unpack(buffer,100,&position,&Arr[(i*n)+i],1,MPI_INT,MPI_COMM_WORLD);
+        }
+        printf("\nMatrix in process 1 after receiving diagonal from process 0:\n");
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                printf("%d ",Arr[(i*n)+j]);
+            }
+            printf("\n");
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+   
+    free(Arr);
+
     MPI_Finalize();
+    printf("\n");
+
     return 0;
 }
